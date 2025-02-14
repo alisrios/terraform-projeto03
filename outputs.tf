f07a231418554bb65d4cb522ffdc3ea6e54f64e8
alisrios <alisrios@gmail.com>
2025-02-14 09:35:44 -0300

    Adiocinando ec2 instance,sg e role iam

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,8 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = aws_subnet.private.*.id
 }
+
+output "instance_ip" {
+  description = "Ip da instancia ec2-n8n"
+  value       = aws_instance.this.public_ip
+}
